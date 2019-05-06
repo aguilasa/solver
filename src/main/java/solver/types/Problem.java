96abc03dc151e3268d9a6a0c5d5b5516316a9b58
Ingmar Aguiar <ingmar.aguiar@senior.com.br>
2019-05-06 16:54:54 -0300

    Create Problem.java

---
+++ src/main/java/solver/types/Problem.java
@@ -0,0 +1,25 @@
+package solver.types;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Problem {
+
+	@Getter
+	@Setter
+	private ProblemType problemType;
+
+	@Getter
+	@Setter
+	private ObjectiveFunction objectiveFunction;
+	@Getter
+	private List<Restriction> restrictions = new LinkedList<>();
+
+	public void addRestriction(Restriction restriction) {
+		restrictions.add(restriction);
+	}
+
+}
