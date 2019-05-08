1a09e8af507a2cd47c8928b3285ba58da14b69f8
Ingmar Aguiar <ingmar.aguiar@senior.com.br>
2019-05-08 11:46:13 -0300

    Ajustes

--- src/main/java/solver/Solver.java
+++ src/main/java/solver/Solver.java
@@ -34,79 +34,79 @@ public class Solver {
 	}
 
 	public static void problems() throws IOException {
-		List<Problem> problems = TypesHelper.loadProblemsFromFile(RESOURCES.concat("input/01.txt"));
-		for (Problem p : problems) {
-			create(p);
-			break;
+		try (XSSFWorkbook workbook = workbook();) {
+			List<Problem> problems = TypesHelper.loadProblemsFromFile(RESOURCES.concat("input/01.txt"));
+			int count = 0;
+			for (Problem p : problems) {
+				create(p, workbook, ++count);
+			}
+			writeWorkbook(workbook, getTemporaryFile("temp"));
 		}
 	}
 
-	public static final void create(Problem problem) throws FileNotFoundException, IOException {
-		try (XSSFWorkbook workbook = workbook();) {
-			Map<Variable, String> variableCells = new LinkedHashMap<>();
-			Sheet sheet = workbook.createSheet("Problema");
-			sheet.setColumnWidth(0, 4000);
+	public static final void create(Problem problem, XSSFWorkbook workbook, int index) throws FileNotFoundException, IOException {
+		Map<Variable, String> variableCells = new LinkedHashMap<>();
+		Sheet sheet = workbook.createSheet(String.format("Problema%s", index));
+		sheet.setColumnWidth(0, 4000);
 
-			int total = problem.getVariables().size() + problem.getRestrictions().size() + 5;
-			for (int i = 0; i < total; i++) {
-				sheet.createRow(i);
-			}
+		int total = problem.getVariables().size() + problem.getRestrictions().size() + 5;
+		for (int i = 0; i < total; i++) {
+			sheet.createRow(i);
+		}
 
-			XSSFCellStyle center = alignCenter(workbook);
-			XSSFCellStyle right = alignRight(workbook);
+		XSSFCellStyle center = alignCenter(workbook);
+		XSSFCellStyle right = alignRight(workbook);
 
-			Row row = sheet.getRow(0);
-			Cell cell = row.createCell(0);
-			cell.setCellStyle(center);
-			cell.setCellValue("Variáveis");
+		Row row = sheet.getRow(0);
+		Cell cell = row.createCell(0);
+		cell.setCellStyle(center);
+		cell.setCellValue("Variáveis");
 
-			int rowNumber = 0;
-			for (Variable v : problem.getVariables()) {
-				row = sheet.getRow(++rowNumber);
-				cell = row.createCell(0);
-				cell.setCellStyle(right);
-				cell.setCellValue(v.getValue());
-				variableCells.put(v, String.format("B%d", rowNumber + 1));
-			}
-
-			rowNumber++;
+		int rowNumber = 0;
+		for (Variable v : problem.getVariables()) {
 			row = sheet.getRow(++rowNumber);
 			cell = row.createCell(0);
 			cell.setCellStyle(right);
-			cell.setCellValue("Função Objetivo");
+			cell.setCellValue(v.getValue());
+			variableCells.put(v, String.format("B%d", rowNumber + 1));
+		}
+
+		rowNumber++;
+		row = sheet.getRow(++rowNumber);
+		cell = row.createCell(0);
+		cell.setCellStyle(right);
+		cell.setCellValue("Função Objetivo");
+
+		cell = row.createCell(1);
+		cell.setCellStyle(right);
+		cell.setCellFormula(getFormulaFromTokens(problem.getObjective().getTokens(), variableCells));
+
+		rowNumber++;
+		row = sheet.getRow(++rowNumber);
+		cell = row.createCell(0);
+		cell.setCellStyle(center);
+		cell.setCellValue("Restrições");
+
+		int i = 0;
+		for (Restriction restriction : problem.getRestrictions()) {
+			row = sheet.getRow(++rowNumber);
+			cell = row.createCell(0);
+			cell.setCellStyle(right);
+			cell.setCellValue(String.format("r%d", ++i));
 
 			cell = row.createCell(1);
 			cell.setCellStyle(right);
-			cell.setCellFormula(getFormulaFromTokens(problem.getObjective().getTokens(), variableCells));
+			cell.setCellFormula(getFormulaFromTokens(restriction.getTokens(), variableCells));
 
-			rowNumber++;
-			row = sheet.getRow(++rowNumber);
-			cell = row.createCell(0);
+			cell = row.createCell(2);
 			cell.setCellStyle(center);
-			cell.setCellValue("Restrições");
+			cell.setCellValue(restriction.getComparation().toString());
 
-			int i = 0;
-			for (Restriction restriction : problem.getRestrictions()) {
-				row = sheet.getRow(++rowNumber);
-				cell = row.createCell(0);
-				cell.setCellStyle(right);
-				cell.setCellValue(String.format("r%d", ++i));
-
-				cell = row.createCell(1);
-				cell.setCellStyle(right);
-				cell.setCellFormula(getFormulaFromTokens(restriction.getTokens(), variableCells));
-
-				cell = row.createCell(2);
-				cell.setCellStyle(center);
-				cell.setCellValue(restriction.getComparation().toString());
-
-				cell = row.createCell(3);
-				cell.setCellStyle(right);
-				cell.setCellValue(restriction.getLimit().getValue());
-			}
-
-			writeWorkbook(workbook, getTemporaryFile("temp"));
+			cell = row.createCell(3);
+			cell.setCellStyle(right);
+			cell.setCellValue(restriction.getLimit().getValue());
 		}
+
 	}
 
 	private static String getFormulaFromTokens(List<Token<?>> tokens, Map<Variable, String> variableCells) {
@@ -114,7 +114,7 @@ public class Solver {
 		for (Token<?> token : tokens) {
 			if (token instanceof Composition) {
 				Composition composition = (Composition) token;
-				sb.append(composition.getValue().toString());
+				sb.append(composition.getValue().toString().replace(",", "."));
 				sb.append("*");
 				sb.append(variableCells.get(composition.getVariable()));
 			} else {
